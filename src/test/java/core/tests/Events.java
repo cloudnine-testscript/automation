50384bb9c6b7b6c50039329e04488a290db4ec26
soubhagya-addvector <soubhagya@addvector.com>
2019-11-20 17:19:40 +0530

    priority added to Events test cases

--- src/test/java/core/tests/Events.java
+++ src/test/java/core/tests/Events.java
@@ -3,7 +3,7 @@ package core.tests;
 import org.testng.annotations.Test;
 
 public class Events extends SupportTest {
-	@Test(enabled = true, priority = 13)
+	@Test(enabled = true, priority = 1)
 	public void tc_UE_Bangalore() throws Exception {
 		logger.debug(this.getTestStartInfoMessage("Account -> Help -> Book Lab Or Scan Start"));
 		lp.login();
